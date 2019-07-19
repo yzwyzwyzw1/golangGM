28819938a85ffb7253cda66ea3972eb4a917664f
yzwyzwyzw <1957855254@qq.com>
2019-07-19 17:34:11 +0800

    second commit

--- util/padding.go
+++ util/padding.go
@@ -13,3 +13,29 @@ func PKCS5UnPadding(src []byte) []byte {
 	unpadding := int(src[length-1])
 	return src[:(length - unpadding)]
 }
+
+func PKCS7Padding(src []byte,blockSize int) []byte {
+	padding := blockSize - len(src)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+//blockSize 密钥长度大小 --->  如 sm4.blockSize
+func PKCS7UnPadding(src []byte,blockSize int) ([]byte, error) {
+	length := len(src)
+	unpadding := int(src[length-1])
+
+
+	if unpadding > blockSize || unpadding == 0 {
+		return nil, errors.New("Invalid pkcs7 padding (unpadding > sm4.BlockSize || unpadding == 0)")
+	}
+
+	pad := src[len(src)-unpadding:]
+	for i := 0; i < unpadding; i++ {
+		if pad[i] != byte(unpadding) {
+			return nil, errors.New("Invalid pkcs7 padding (pad[i] != unpadding)")
+		}
+	}
+
+	return src[:(length - unpadding)], nil
+}
